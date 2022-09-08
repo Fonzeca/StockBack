5ddd438ab07186263ceb9dfded2508a4d7581c9e
Pelli <martinpelli@outlook.com>
2022-09-08 10:22:45 -0300

    /getAllProducts

---
+++ src/db/model/dynamic_data.go
@@ -0,0 +1,8 @@
+package model
+
+type Product struct {
+	Id           int32  `json:"id"`
+	Nombre       string `json:"nombre"`
+	IdContenedor int32  `json:"id_contenedor,omitempty"`
+	Cantidad     int16  `json:"cantidad"`
+}
